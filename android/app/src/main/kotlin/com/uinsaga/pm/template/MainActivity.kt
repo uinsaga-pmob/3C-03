57b97bbf50fa9dc292d8645babe440226d4443ab
yogaariz <yogaariz1234@gmail.com>
2026-05-06 14:50:45 +0700

    teerbaru

--- android/app/src/main/kotlin/com/uinsaga/pm/template/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.uinsaga.pm.template
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/pages/history_page.dart
+++ lib/pages/history_page.dart
@@ -204,7 +204,7 @@ class HistoryPage extends StatelessWidget {
                       ],
                     ),
                   );
-                }).toList(),
+                }),
               ],
             ),
     );
